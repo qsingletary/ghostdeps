8976c0a1f4faef3c175d6a5bc4d003e88ae512dc
Quinn Singletary <qsingletary@proton.me>
2026-01-19 00:14:02 -0500

    chore: update configurations for monorepo structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./common/**/*.{js,ts,jsx,tsx}",
+    "./modules/**/*.{js,ts,jsx,tsx}",
+    "./stores/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
   plugins: [],
 };
 
